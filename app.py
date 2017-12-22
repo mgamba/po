d77f7b1131dddf7f549aa5687b266adee2e8387d
Michael Gamba <track.tune.seek@gmail.com>
2017-12-21 22:35:58 -0500

    Fetch dataframe from sqlite db

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 import falcon
-import pandas
+import pandas as pd
+
+import db
 
 class Home:
     def on_get(self, req, resp):
         header = ['Kung Foo Master','Style']
-        data = [['Master Po Ping', 'Improvised and Panda Kung Fu'],
-                ['Master Shifu', 'Red Panda Kung Fu'],
-                ['Grand Master Oogway', 'Shaolinquan and T\'ai chi ch\'uan'],
-                ['Master Tigress', 'Black Tiger Kung Fu'],
-                ['Master Viper', 'Viper Kung Fu'],
-                ['Master Monkey', 'Monkey Kung Fu'],
-                ['Master Mantis', 'Southern Praying Mantis Kung Fu'],
-                ['Master Crane', 'Fujian White Crane Kung Fu']]
 
-        df = pandas.DataFrame(data, columns=header)
+        df = pd.read_sql_query('select * from masters', con=db.engine)
         html = df.to_html()
 
         resp.content_type = falcon.MEDIA_HTML
