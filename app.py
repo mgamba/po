8737c7bde7d2e2578d81d98af12e141f9c0a2aa9
Michael Gamba <track.tune.seek@gmail.com>
2017-12-21 21:24:24 -0500

    Serve pandas dataframe as html table

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import falcon
+import pandas
 
 class Home:
     def on_get(self, req, resp):
-        html = """
-                <!DOCTYPE html>
-                <html>
-                <body>
-                  <h1>Hello.</h1>
-                </body>
-                </html>
-               """
+        header = ['Kung Foo Master','Style']
+        data = [['Master Po Ping', 'Improvised and Panda Kung Fu'],
+                ['Master Shifu', 'Red Panda Kung Fu'],
+                ['Grand Master Oogway', 'Shaolinquan and T\'ai chi ch\'uan'],
+                ['Master Tigress', 'Black Tiger Kung Fu'],
+                ['Master Viper', 'Viper Kung Fu'],
+                ['Master Monkey', 'Monkey Kung Fu'],
+                ['Master Mantis', 'Southern Praying Mantis Kung Fu'],
+                ['Master Crane', 'Fujian White Crane Kung Fu']]
+
+        df = pandas.DataFrame(data, columns=header)
+        html = df.to_html()
 
         resp.content_type = falcon.MEDIA_HTML
         resp.body = html
